b10dbc900fedb1bdc9c6393a050efd200468d436
dianewudw <diane@Dianes-Air.localdomain>
2023-08-26 11:11:50 -0700

    fixed homepage justify end

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -12,13 +12,13 @@ export default function Home() {
                         <span >
                         <h1 class='flex flex-start whitespace-nowrap font-display italic text-4xl md:text-9xl mb--8 text-gray'>It's Diane</h1>
                         <span class='flex flex-start direction-row'>
-    <img className='flex self-start md:pl-20' src='/Squiggle.svg'
-         alt="Name Underline"/>
+                            <img className='flex self-start md:pl-20' src='/Squiggle.svg'
+                                 alt="Name Underline"/>
                         </span>
                         </span>
                     </div>
             </div>
-            <div className='flex px-5 pt-20 md:pt-40 pb-10 md:px-20'>
+            <div className='flex px-5 pt-20 md:pt-40 pb-10 md:px-20 justify-end'>
                 <div className='flex flex-col justify-items-end'>
                     <img src='/flower_profile.svg' alt='portfolio_image' className='w-fit self-end'/>
                     <p className='font-body  text-2xl md:text-6xl text-end text-gray sm:py-20 pl-10 lg:pl-0'>Co-founder,
